3c0136d1b0030d7a524bf469165b7038743f73a5
Elian <elianmdnazueta@gmail.com>
2023-08-05 17:17:13 +0000

    tweaked role tasks

---
+++ roles/base-machine/files/nvim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+nvim_dir='/home/elian/.config/nvim'
+
+if [ -d $nvim_dir ]; then
+
+	read -p "~/.config/nvim found, continue? (y/n) > " -n 1 -r
+
+	echo   
+
+	if [[ ! $REPLY =~ ^[Yy]$ ]]
+	then
+	    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+	fi
+
+	
+	echo "deleting..."
+	sudo rm -r /home/elian/.config/nvim/
+	echo "cloning repository..."
+	git clone https://github.com/elianmanzueta/nvim.git /home/elian/.config/nvim
+else
+	echo "nvim directory not found, installing configuration..."
+	git clone https://github.com/elianmanzueta/nvim.git /home/elian/.config/nvim
+fi
